a342a00f05b7bb1527048cf429ac9c52448e21b6
Kientufts <letrungkien1991@gmail.com>
2018-08-31 20:16:29 -0700

    create all entities

---
+++ src/com/iansky/entermark19/manager/BookmarkManager.java
@@ -0,0 +1,13 @@
+package com.iansky.entermark19.manager;
+
+public class BookmarkManager {
+	private static BookmarkManager instance = new BookmarkManager();
+	
+	private BookmarkManager() {}
+	
+	public static BookmarkManager getInstance() {
+		return instance;
+	}
+	
+	public Movie 
+}
