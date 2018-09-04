5462c88cc3eb00bcdff960b72d65b83e3e168404
Kientufts <letrungkien1991@gmail.com>
2018-09-04 01:11:38 -0700

    IOUtil processing

---
+++ src/com/iansky/entermark19/util/IOUtil.java
@@ -0,0 +1,29 @@
+package com.iansky.entermark19.util;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+public class IOUtil {
+	public static void read(String[] data, String filename) {
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filename), "UTF-8"))){
+			String line;
+			int count = 0;
+			while((line = br.readLine()) != null) {
+				data[count++] = line;
+			}
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
